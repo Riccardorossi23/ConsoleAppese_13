429a218ecad00a69d27bdf6bb928169c3776f13f
Riccardorossi23 <55913260+Riccardorossi23@users.noreply.github.com>
2019-10-20 10:29:38 +0200

    Add project files.

---
+++ ConsoleAppese_13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppese_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il prezzo: ");
+            int spesa1 = int.Parse(Console.ReadLine());
+            if (spesa1 > 500)
+            {
+                double sconto = spesa1 * 20 / 100;
+                double totale = spesa1 - sconto;
+                Console.WriteLine($"il totale è più conveniente il primo negozio: {totale}");
+            }
+            else if (spesa1 > 300)
+            {
+                double sconto = spesa1 * 20 / 100;
+                double totaleMaggiore = spesa1 - sconto;
+                Console.WriteLine($" il totale è più convenienza al secondo negozio: {totaleMaggiore}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
+            
